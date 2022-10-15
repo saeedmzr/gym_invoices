104ac1362a9de175a3728c6930dca54065e415d7
saeedmouzarmi <saeedmouzarmi@gmail.com>
2022-10-15 17:37:49 +0330

    add user invoices web service

---
+++ database/migrations/2022_10_15_113747_create_checkins_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checkins', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\Invoice::class);
+            $table->foreignIdFor(\App\Models\Club::class);
+            $table->enum('type', ['Guest', 'Membership'])->default('Guest');
+            $table->integer('amount')->default(0);
+            $table->longText('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checkins');
+    }
+};
